5a0dafa3b151e818b02d0d21be778ff77ff506ed
Roger Wechsler <mr.mutator@gmail.com>
2015-06-08 16:47:43 +0200

    Updates.

--- compounds.py
+++ compounds.py
@@ -6,9 +6,6 @@ import multiprocessing as mp
 import time
 import cPickle as pickle
 
-def dd():
-    return defaultdict(int)
-
 
 def build_vocabulary(corpus, min_length=0):
 
@@ -29,19 +26,6 @@ def get_combinations(vocab, threads=4):
 
     heads = vocab.keys()
 
-
-    # processes = [mp.Process(target=try_combinations, args=(heads[i:j], vocab, output)) for i, j in splits]
-    #
-    # for p in processes:
-    #     p.start()
-    #
-    # print "running"
-    #
-    # for p in processes:
-    #     p.join()
-    #
-    # print "done"
-
     pool = mp.Pool(processes=threads)
 
     results = [pool.apply_async(try_combinations, args=(heads[i:j], vocab)) for i, j in splits]
@@ -50,41 +34,36 @@ def get_combinations(vocab, threads=4):
 
 
     combinations = dict()
-    compound_dict = dict()
 
-    for i, tup in enumerate(output):
-        combinations.update(tup[0])
-        compound_dict.update(tup[1])
+    for comb_dict in output:
+        combinations.update(comb_dict)
 
-    return combinations, compound_dict
+    return combinations
 
 
 
 def try_combinations(heads, vocab):
-    combinations = defaultdict(dd)
-    compound_dict = defaultdict(int)
+    combinations = defaultdict(set)
     for w in vocab:
-        if w[0].upper() == w[0]:
-            for head in heads:
-                head= head.lower()
-                combo = vocab.get(w + head, 0)
-                if combo:
-                    combinations[head][w] += 1
-                    compound_dict[w + "-" + head] += 1
+        for head in heads:
+            head= head.lower()
+            combo = vocab.get(w + head, 0)
+            if combo:
+                combinations[head].add(w)
 
-    return combinations, compound_dict
+    return combinations
 
 
 if __name__ == "__main__":
 
     start = time.time()
 
-    corpus = CorpusReader("data/europarl-v7.de.gz", max_limit=200)
+    corpus = CorpusReader("data/europarl-v7.de.gz", max_limit=1000)
     print "building vocab"
     vocab = build_vocabulary(corpus, min_length=4)
     print len(vocab)
     print "building combinations"
-    combinations, compound_dict = get_combinations(vocab, threads=5)
+    combinations = get_combinations(vocab, threads=8)
     print "combinations built"
     # print most frequent combinations:
 
@@ -92,10 +71,8 @@ if __name__ == "__main__":
 
     print "pickling"
 
-    pickle.dump([combinations, compound_dict], open("output.p", "wb"))
+    pickle.dump(combinations, open("output.p", "wb"))
 
-
-    for compound in sorted(compound_dict, key=compound_dict.get, reverse=True)[:50]:
-        print compound, compound_dict[compound]
+    print combinations
 
     print "Time: ", end-start
