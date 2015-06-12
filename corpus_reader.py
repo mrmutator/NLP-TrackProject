ab1a47d273d995a0b5c6d5ca5ab26f32ac47cd5d
Roger Wechsler <mr.mutator@gmail.com>
2015-06-12 17:27:13 +0200

    Fixes.

--- corpus_reader.py
+++ corpus_reader.py
@@ -9,40 +9,24 @@ class CorpusReader():
     Reads corpus from gzip file.
     """
 
-    def __init__(self, file_name, max_limit=None):
-        self.file = gzip.open(file_name, "rb")
-        self.max_limit = max_limit
+    def __init__(self, files, max_limit=None):
+        if isinstance(files, str):
+            self.files = [files]
+        else:
+            self.files = files
         self.tokenizer = RegexpTokenizer(r'\w+')
-
+        self.max_limit = max_limit
 
     def __iter__(self):
         """
         Generator that returns a list of tokens for each sentence.
         :return: list of tokens
         """
-        self.file.seek(0)
-        c = 0
-        for line in self.file:
-            c += 1
-            if self.max_limit and c > self.max_limit:
-                break
-            yield self.tokenizer.tokenize(line.decode("utf-8"))
-
-    def get_raw_corpus(self):
-        """ Generator that returns one sentence each time as a string (untokenized)
-        :return:one sentence (string)
-        """
-        self.file.seek(0)
-        c = 0
-        for line in self.file:
-            c += 1
-            if self.max_limit and c > self.max_limit:
-                break
-            yield line.decode("utf-8")
-
-
-if __name__ == "__main__":
-    corpus = CorpusReader("data/europarl-v7.de.gz", max_limit=100)
-
-    for tokens in corpus:
-        print tokens
+        for f in self.files:
+            print "Processing ", f
+            c = 0
+            for line in gzip.open(f, "rb"):
+                c += 1
+                if c > self.max_limit:
+                    break
+                yield self.tokenizer.tokenize(line)
--- train_word2vec.py
+++ train_word2vec.py
@@ -7,26 +7,13 @@ from corpus_reader import CorpusReader
 
 
 
-class Dataset():
-
-    def __init__(self, files):
-        self.files = files
-
-    def __iter__(self):
-        for f in self.files:
-            print "Reading file ", f
-            cr = CorpusReader(f, max_limit=10)
-            for sentence in cr:
-                yield sentence
-
-
 files = glob.glob(sys.argv[1])
 outfile_name = sys.argv[2]
 
-
-dataset = Dataset(files)
+dataset = CorpusReader(files, max_limit=20)
 
 model = gensim.models.Word2Vec(dataset, size=500, window=5, min_count=3, negative=5, workers=2)
+
 
 model.save(outfile_name)
 
