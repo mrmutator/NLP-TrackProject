0ba58959323c2ac9e46fba3e4ecfa1d716d8fbe2
Roger Wechsler <mr.mutator@gmail.com>
2015-06-12 17:05:49 +0200

    Fixes iterator.

--- train_word2vec.py
+++ train_word2vec.py
@@ -5,19 +5,26 @@ import sys
 import glob
 from corpus_reader import CorpusReader
 
-def create_dataset(files):
-    for f in files:
-        print "Reading file ", f
-        cr = CorpusReader(f, max_limit=10)
-        for sentence in cr:
-            yield sentence
+
+
+class Dataset():
+
+    def __init__(self, files):
+        self.files = files
+
+    def __iter__(self):
+        for f in self.files:
+            print "Reading file ", f
+            cr = CorpusReader(f, max_limit=10)
+            for sentence in cr:
+                yield sentence
 
 
 files = glob.glob(sys.argv[1])
 outfile_name = sys.argv[2]
 
 
-dataset = create_dataset(files)
+dataset = Dataset(files)
 
 model = gensim.models.Word2Vec(dataset, size=500, window=5, min_count=3, negative=5, workers=2)
 
