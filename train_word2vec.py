38cb29183e78f02dec692cc16182d14912a1bd2b
Roger Wechsler <mr.mutator@gmail.com>
2015-06-12 17:35:54 +0200

    Fixes.

--- train_word2vec.py
+++ train_word2vec.py
@@ -10,7 +10,7 @@ from corpus_reader import CorpusReader
 files = glob.glob(sys.argv[1])
 outfile_name = sys.argv[2]
 
-dataset = CorpusReader(files, max_limit=20)
+dataset = CorpusReader(files)
 
 model = gensim.models.Word2Vec(dataset, size=500, window=5, min_count=3, negative=5, workers=2)
 
