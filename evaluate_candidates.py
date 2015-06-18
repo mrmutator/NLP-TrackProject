94b2f2e06b49d3d4994eca55ace9114e5f431ee8
Roger Wechsler <mr.mutator@gmail.com>
2015-06-18 12:59:36 +0200

    Removed word2vec models. Only indices.

--- evaluate_candidates.py
+++ evaluate_candidates.py
@@ -9,6 +9,7 @@ import sys
 import argparse
 import time
 import datetime
+import numpy as np
 
 
 def timestamp():
@@ -20,20 +21,9 @@ def load_candidate_dump(file_name):
 def load_word2vecmodel(file_name):
     return gensim.models.Word2Vec.load_word2vec_format(file_name, binary=True)
 
-def build_annoy_tree(word2vec_model,  output_file_name, n_trees=100):
-    tree = AnnoyIndex(word2vec_model.layer1_size)
-    for i, word in enumerate(word2vec_model.index2word):
-        tree.add_item(i, list(word2vec_model[word]))
 
-    tree.build(n_trees)
-
-
-    tree.save(output_file_name)
-
-    return output_file_name
-
-def load_annoy_tree(model_file_name, word2vec_model):
-    tree = AnnoyIndex(word2vec_model.layer1_size)
+def load_annoy_tree(model_file_name, vector_dims):
+    tree = AnnoyIndex(vector_dims)
     tree.load(model_file_name)
     return tree
 
@@ -44,30 +34,20 @@ def annoy_knn(annoy_tree, vector, true_index, k=100):
     else:
         return False
 
-def word2vec_knn(word2vec_model, vector, true_word, k=100):
-    neighbours, _ = zip(*word2vec_model.most_similar([vector], topn=k))
-    if true_word in neighbours:
-        return True
-    else:
-        return False
 
-
-def evaluate_set(prefix, tails, word2vec_model, annoy_tree_file, rank_threshold=100, sample_size=1000):
+def evaluate_set(prefix, tails, annoy_tree_file, vector_dims, rank_threshold=100, sample_size=1000):
     counts = dict()
     counts[True] = 0
     counts[False] = 0
-    annoy_tree = load_annoy_tree(annoy_tree_file, word2vec_model)
+    annoy_tree = load_annoy_tree(annoy_tree_file, vector_dims)
     if len(tails) > sample_size:
         tails = random.sample(tails, sample_size)
-    for (fl1, tail1), (fl2, tail2) in itertools.combinations(tails, 2):
+    for (comp1, tail1), (comp2, tail2) in itertools.combinations(tails, 2):
         try:
-            diff = word2vec_model[prefix + fl2 + tail2.lower()] - word2vec_model[tail2]
-            predicted = word2vec_model[tail1] + diff
-            true_word = prefix + fl1 + tail1.lower()
-            true_index = word2vec_model.vocab[true_word].index
+            diff = np.array(annoy_tree.get_item_vector(comp2))- np.array(annoy_tree.get_item_vector(tail2))
+            predicted = np.array(annoy_tree.get_item_vector(tail1)) + diff
 
-            result = annoy_knn(annoy_tree, predicted, true_index, rank_threshold)
-            #result = word2vec_knn(word2vec_model, predicted, true_word, rank_threshold)
+            result = annoy_knn(annoy_tree, predicted, comp1, rank_threshold)
 
             counts[result] += 1
 
@@ -108,18 +88,18 @@ def test_pair(pair1, pair2, word2vec_model, k=100, show=30):
     print "Found: ", true_word in neighbours
 
 
-def candidate_generator(candidates, annoy_tree_file, word2vec_model, rank_threshold, sample_size):
+def candidate_generator(candidates, annoy_tree_file, vector_dims, rank_threshold, sample_size):
     for prefix in candidates:
-        yield (prefix, candidates[prefix], word2vec_model, annoy_tree_file, rank_threshold, sample_size)
+        yield (prefix, candidates[prefix], annoy_tree_file, vector_dims, rank_threshold, sample_size)
 
 def mp_wrapper_evaluate_set(argument):
     return evaluate_set(*argument)
 
 
-def evaluate_candidates(candidates, annoy_tree_file, word2vec_model, rank_threshold=100, sample_size=500, processes=4):
+def evaluate_candidates(candidates, annoy_tree_file, vector_dims, rank_threshold=100, sample_size=500, processes=4):
     pool = mp.Pool(processes=processes)
 
-    arguments = candidate_generator(candidates, annoy_tree_file, word2vec_model, rank_threshold, sample_size)
+    arguments = candidate_generator(candidates, annoy_tree_file, vector_dims, rank_threshold, sample_size)
     results = pool.map(mp_wrapper_evaluate_set, arguments)
 
     return results
@@ -136,13 +116,10 @@ if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser(description='Evaluate candidates')
-
-    parser.add_argument('-w', action="store", dest="word2vec_file", required=True)
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-t', action="store", dest="annoy_tree_file")
-    group.add_argument('-b', action="store", dest="output_tree_file")
-    parser.add_argument('-c', action="store", dest="candidates_file")
-    parser.add_argument('-o', action="store", dest="result_output_file")
+    parser.add_argument('-d', action="store", dest="vector_dims", type=int, required=True)
+    parser.add_argument('-t', action="store", dest="annoy_tree_file")
+    parser.add_argument('-c', action="store", dest="candidates_index_file", required=True)
+    parser.add_argument('-o', action="store", dest="result_output_file", required=True)
     parser.add_argument('-p', action="store", dest="n_processes", type=int, default=n_processes)
     parser.add_argument('-n', action="store", dest="n_annoy_trees", type=int, default=n_annoy_trees)
     parser.add_argument('-s', action="store", dest="sample_set_size", type=int, default=sample_set_size)
@@ -150,28 +127,16 @@ if __name__ == "__main__":
 
     arguments = parser.parse_args(sys.argv[1:])
 
-    print timestamp(), "loading word2vec model"
-    word2vec_model = load_word2vecmodel(arguments.word2vec_file)
 
-    if not arguments.annoy_tree_file:
+    print timestamp(), "loading candidates"
+    candidates = load_candidate_dump(arguments.candidates_index_file)
 
-        print timestamp(), "building annoy tree"
-        annoy_tree_file = build_annoy_tree(word2vec_model, n_trees=arguments.n_annoy_trees, output_file_name=arguments.output_tree_file)
+    print timestamp(), "evaluating candidates"
+    results = evaluate_candidates(candidates, arguments.annoy_tree_file, arguments.vector_dims, rank_threshold=arguments.rank_threshold,
+                                  sample_size=arguments.sample_set_size, processes=arguments.n_processes)
 
-    else:
-        print timestamp(), "loading annoy tree"
-        annoy_tree_file = arguments.annoy_tree_file
-
-    if arguments.candidates_file and arguments.result_output_file:
-        print timestamp(), "loading candidates"
-        candidates = load_candidate_dump(arguments.candidates_file)
-
-        print timestamp(), "evaluating candidates"
-        results = evaluate_candidates(candidates, annoy_tree_file, word2vec_model, rank_threshold=arguments.rank_threshold,
-                                      sample_size=arguments.sample_set_size, processes=arguments.n_processes)
-
-        print timestamp(), "pickling"
-        pickle.dump(results, open(arguments.result_output_file, "wb"))
+    print timestamp(), "pickling"
+    pickle.dump(results, open(arguments.result_output_file, "wb"))
 
     print timestamp(), "done"
 
