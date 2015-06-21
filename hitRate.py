92fd12194069eb58f3681ccb9d5f796c717bbd14
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2015-06-21 05:15:51 +0200

    list hit rates

---
+++ hitRate.py
@@ -0,0 +1,21 @@
+__author__ = 'lqrz'
+
+import pickle
+import glob
+import codecs
+
+if __name__ == '__main__':
+    fnames = glob.glob('output*/results.p')
+
+    fout = codecs.open('hitRates.txt', 'w', 'utf-8')
+
+    results = []
+    for fPath in fnames:
+        results.extend(pickle.load(open('results.p', 'r')))
+
+    orderedResults = sorted([(k, r) for k,r in results], key=lambda t: t[1], reverse=True)
+    
+    for k,r in orderedResults:
+        fout.write(k+'\t'+str(r)+'\n')
+
+    fout.close()
