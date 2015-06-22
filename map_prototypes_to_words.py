f9abb6559e2c3b593615b83992bc448a34819932
Roger Wechsler <mr.mutator@gmail.com>
2015-06-22 22:52:31 +0200

    Mapping from index to words.

---
+++ map_prototypes_to_words.py
@@ -0,0 +1,37 @@
+__author__ = 'rwechsler'
+import gensim
+import cPickle as pickle
+import argparse
+import sys
+import codecs
+
+
+def load_word2vecmodel(file_name):
+    return gensim.models.Word2Vec.load_word2vec_format(file_name, binary=True)
+
+
+def load_prototype_dump(file_name):
+    return pickle.load(open(file_name, "rb"))
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Evaluate candidates')
+    parser.add_argument('-w', action='store', dest="word2vec_file", required=True)
+    parser.add_argument('-p', action='store', dest='prototype_file', required=True)
+    parser.add_argument('-o', action="store", dest='output_file' required=True)
+
+    arguments = parser.parse_args(sys.argv[1:])
+
+    word2vec_model = load_word2vecmodel(arguments.word2vec_file)
+    prototype_set = load_prototype_dump(arguments.prototype_file)
+
+    outfile = codecs.open(arguments.output_file, "w", "utf-8")
+
+    for candidate in prototype_set:
+        prototypes = prototype_set[candidate]
+        for prototype, evidence_set in prototypes:
+            outfile.write(candidate + "\t" + word2vec_model.index2word[prototype] + "\t" + " ".join([word2vec_model.index2word[i] for i in evidence_set]) + "\n")
+
+    outfile.close()
+
