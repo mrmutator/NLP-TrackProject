959b4d7f6a47cdbbbd5a2d086781b38dc971e6cd
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2015-06-28 18:57:46 +0200

    changes according to last decompound modif

--- evaluateDecompounding.py
+++ evaluateDecompounding.py
@@ -12,10 +12,10 @@ logger.addHandler(hdlr)
 logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
-    goldFile = 'decompounding/lqrz_compounds.test' # has to start with 2 lines of header
-    # # resultsFiles = glob.glob('/home/lquiroz/jobs/decompound/100_046/output*/results.txt')
-    resultsFiles = glob.glob('output*/results.txt')
-    resultsFolder = 'decompounding' # without last /
+    # goldFile = 'decompounding/lqrz_compounds.test' # has to start with 2 lines of header
+    # # # resultsFiles = glob.glob('/home/lquiroz/jobs/decompound/100_046/output*/results.txt')
+    # resultsFiles = glob.glob('output*/results.txt')
+    # resultsFolder = 'decompounding' # without last /
 
     if len(sys.argv) == 3:
         goldFile = sys.argv[1]
@@ -35,6 +35,8 @@ if __name__ == '__main__':
 
     coverage = 0
 
+    noInputRepresentation = 0
+    noTailRepresentation = 0
     noSplitsAtAll = 0
     discardedSplits = 0
 
@@ -47,28 +49,39 @@ if __name__ == '__main__':
         for l in fresults:
             totalResults += 1
             cleanLine = l.strip('\n').split('\t')
-            resultSplit = ' '.join([cleanLine[1], cleanLine[2]]).strip()
-            resultsCompounds[cleanLine[0]] = resultSplit
-            if cleanLine[1] == '' and cleanLine[2] == '':
-                # We didnt find proper prefixes (either no prefix matched or the tails were not in the w2v model)
+
+            if cleanLine[1] == 'Noinputrep':
+                # We didnt find a representation for the compound
+                resultsCompounds[cleanLine[0]] = ''
+                noInputRepresentation += 1
                 noSplitsAtAll += 1
+                continue
+            elif cleanLine[1] == 'Notailrep':
+                # We didnt find a representation for the substring
+                resultsCompounds[cleanLine[0]] = ''
+                noTailRepresentation += 1
+                noSplitsAtAll +=1
                 continue
             elif cleanLine[0] == cleanLine[1] and cleanLine[2] == '':
                 # We found possible splits, but they didnt pass rank and similarity thresholds
+                resultsCompounds[cleanLine[0]] = ''
                 discardedSplits += 1
                 continue
 
+            # We were able to split
+            resultSplit = ' '.join([cleanLine[1], cleanLine[2]]).strip()
+            resultsCompounds[cleanLine[0]] = resultSplit
             coverage += 1
 
     lineNr = 0
     for l in fgold:
         lineNr += 1
-        # if lineNr < 3:
+        # Gold file must have 2 header lines
         if lineNr < 3:
             continue
         goldSplit = set()
         idx = l.strip('\n').find('|')
-        if idx <>-1:
+        if idx != -1:
             # multiple possibilities
             compound = l.strip('\n').split('\t')[0].strip()
             modifier1 = l.strip('\n').split('\t')[1].split('|')[0].strip()
@@ -89,13 +102,21 @@ if __name__ == '__main__':
     assert lineNr-2 == totalResults, 'Total nr of lines in gold file does not match total nr lines in results file '+\
         str(lineNr-2)+' '+str(totalResults)
 
+    assert noSplitsAtAll == (noTailRepresentation+noInputRepresentation), 'Error in nr of no splits.'
+
     # Stats
     logger.info('Total number of examples: '+str(lineNr-2))
-    logger.info('Coverage: '+str(coverage)+' '+str(coverage/float(lineNr-2)))
+    logger.info('Coverage: '+str(coverage)+' '+str(coverage/float(lineNr-2))) # Measured against all word in gold file.
+
     logger.info('Examples for which no splits were found: '+str(noSplitsAtAll)+' '+\
                 str(noSplitsAtAll/float(lineNr-2)))
+    logger.info('No input representation found: '+str(noInputRepresentation)+' '+\
+                str(noSplitsAtAll/float(lineNr-2)))
+    logger.info('No tail representation found: '+str(noTailRepresentation)+' '+\
+                str(noSplitsAtAll/float(lineNr-2)))
+
     logger.info('Examples for which weak prefixes were found: '+str(discardedSplits)+' '+\
-                str(discardedSplits/float((lineNr-2))))
-    logger.info('Accuracy: '+str(accuracy/float(lineNr-2)))
+                str(discardedSplits/float((coverage)))) # Measured against split compounds.
+    logger.info('Accuracy: '+str(accuracy/float(coverage))) # Measured against split compounds.
 
     logger.info('End')
